d9083e7fbc0d72729231deed1341372c9866a910
James Swanick <swanijam@gmail.com>
2017-12-15 11:02:38 -0500

    Fixing Dialog UI placement (is probably going to be hidden by other geometry  now) and quickly implementing gobble guy and fireball

---
+++ Assets/_scripts/MoveDialogUIButtonsAndLookAtPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveDialogUIButtonsAndLookAtPlayer : MonoBehaviour {
+	public Transform buttonsPositionPivot;
+	public Transform moveToPoint;
+
+	public void SetDialogButtonsPositionTo(){
+//		if (buttonsPositionPivot != null) {
+//			buttonsPositionPivot = DialogUIManager.instance.mainButtonsPanel.transform.parent;
+//		}
+//		if (moveToPoint != null) {
+//			buttonsPositionPivot.position = moveToPoint.position;
+//		} else {
+//			buttonsPositionPivot.position = transform.position;
+//	}
+		buttonsPositionPivot.position = moveToPoint.position;
+		buttonsPositionPivot.LookAt (Camera.main.transform.position);
+	}
+}
